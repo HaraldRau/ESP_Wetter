075bdcad0a9f2bd0e3b94a086ddeedcae89dfa45
Harald Rau <47145786+HaraldRau@users.noreply.github.com>
2026-09-07 17:03:55 +0200

    Add files via upload

--- User_Setups/User_Setup_Wetter.h
+++ User_Setups/User_Setup_Wetter.h
@@ -1,1 +1,22 @@
+//Abgelegt im Ordner User_Setups
+#define ST7789_DRIVER
 
+#define TFT_WIDTH  240
+#define TFT_HEIGHT 320
+
+#define TFT_MOSI 23
+#define TFT_SCLK 18
+
+#define TFT_CS   5
+#define TFT_DC   4
+#define TFT_RST  2
+
+#define LOAD_GLCD
+#define LOAD_FONT2
+#define LOAD_FONT4
+#define LOAD_FONT6
+#define LOAD_FONT7
+#define LOAD_FONT8
+#define LOAD_GFXFF
+
+#define SPI_FREQUENCY 40000000
